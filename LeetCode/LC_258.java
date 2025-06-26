511944dff373d0917012e68a6a03b251446c6c72
zero <zero@zero.zero>
2025-06-26 08:43:54 +0530

    Day 2 DSA Learning

---
+++ LeetCode/LC_258.java
@@ -0,0 +1,18 @@
+package LeetCode;
+
+public class LC_258 {
+
+    public static void main(String args[]){
+        int num=21893;
+        while(num>9){
+            int sum=0;
+            while(num>0){
+                sum+=num%10;
+                num/=10;
+            }
+            num=sum;
+        }
+        System.out.println(num);
+    }
+    
+}
